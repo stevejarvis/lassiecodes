378a813f492d1ac1ef14c2159adaa9a2e21e7ac8
Steve Jarvis <sajarvis@bu.edu>
2022-01-01 22:31:39 -0600

    Add download button. Solid background.

--- components/qrcode.js
+++ components/qrcode.js
@@ -14,17 +14,27 @@ function QrCode({ primaryContact, subjectName }) {
   const qrOptions = {
     errorCorrectionLevel: 'H'
   }
+
   useEffect(() => {
     let mainCanvas = document.getElementById(canvasId)
     let helperCanvas = document.getElementById(helperCanvasId)
     let ctx = mainCanvas.getContext("2d")
     let helperCtx = helperCanvas.getContext("2d")
     ctx.font = "10px Arial"
+    ctx.clearRect(0, 0, width, height); 
+    // Canvas drawing happens sequentially, really think of putting paint on
+    // a canvas. Everything is layered. So gotta switch colors again after filling.
+    ctx.rect(0, 0, width, height);
+    ctx.fillStyle = "white"
+    ctx.fill()
+    ctx.fillStyle = "black"
+
     if (!primaryContact || !primaryContact.number) {
-      ctx.fillText("(Set emergency contact info to render QR code)",10,70)
+      ctx.fillText("(Set emergency contact info to render QR code)", 10, 70)
+      visibleDownloadBtn(false)
       return
     }
-    ctx.clearRect(0, 0, width, height); 
+
     // For some reason QRCode resizes the canvas. And just changing it back distorts the text badly.
     // So we gotta do this crazy thing with two canvases and draw one into the other.
     // https://github.com/soldair/node-qrcode/blob/f08fd572d7cca92c8b9d71b24cebccf61663d4a6/lib/renderer/canvas.js#L6
@@ -39,7 +49,27 @@ function QrCode({ primaryContact, subjectName }) {
     // Finally, put the helper canvas with the actual code into the main canvas with
     // the words.
     ctx.putImageData(imageData, 5, 5)
+
+    visibleDownloadBtn(true)
   })
+
+  const downloadImage = (e) => {
+    let mainCanvas = document.getElementById(canvasId)
+    let image = mainCanvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+    let link = document.createElement('a');
+    link.download = "qrcode.png";
+    link.href = image;
+    link.click();
+  }
+
+  const visibleDownloadBtn = (visible) => {
+    var x = document.getElementById("downloadButton")
+    if (!visible) {
+      x.style.display = "none"
+    } else {
+      x.style.display = "block"
+    }
+  } 
 
   return (
     <div>
@@ -48,6 +78,14 @@ function QrCode({ primaryContact, subjectName }) {
           {qrcodeStyles}
         </style>
       </canvas>
+      <button id="downloadButton" onClick={(e) => downloadImage(e)}>
+        Download
+        <style jsx>{`
+          button {
+            display: none;
+          }
+        `}</style>
+      </button>
       <canvas id={helperCanvasId}>
         <style jsx>{`
           canvas {
